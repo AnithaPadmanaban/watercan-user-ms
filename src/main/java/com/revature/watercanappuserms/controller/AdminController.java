b114d575e3d23c84ca392f1f2813485c3aaa92ff
Anitha <anithanambi2000@gmail.com>
2019-10-30 15:56:42 +0530

    added requset body in admincontroller

--- src/main/java/com/revature/watercanappuserms/controller/AdminController.java
+++ src/main/java/com/revature/watercanappuserms/controller/AdminController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.revature.watercanappuserms.dto.AdminLoginInfo;
 import com.revature.watercanappuserms.dto.Message;
@@ -23,7 +24,7 @@ public class AdminController {
 	@ApiOperation("AdminLoginApi")
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Admin Login Success", response = Message.class),
 			@ApiResponse(code = 400, message = "Admin Login Failure") })
-	public ResponseEntity<?> adminLogin(AdminLoginInfo adminLoginInfo) {
+	public ResponseEntity<?> adminLogin(@RequestBody AdminLoginInfo adminLoginInfo) {
 		String errorMessage = null;
 		Admin admin = null;
 		try {
