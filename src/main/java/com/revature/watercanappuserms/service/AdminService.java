06471d220adddf03a1d2f1e4e1419ed608725a3f
Anitha <anithanambi2000@gmail.com>
2019-10-30 15:46:40 +0530

    updated

--- src/main/java/com/revature/watercanappuserms/service/AdminService.java
+++ src/main/java/com/revature/watercanappuserms/service/AdminService.java
@@ -1,7 +1,5 @@
 package com.revature.watercanappuserms.service;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.revature.watercanappuserms.dto.AdminLoginInfo;
@@ -14,7 +12,6 @@ public class AdminService {
 	@Autowired
 	AdminRepository adminRepository;
 
-	@Transactional
 	public Admin adminLoginProcess(AdminLoginInfo adminLoginInfo) throws ServiceException {
 		Admin admin = null;
 		String email = adminLoginInfo.getEmail();
